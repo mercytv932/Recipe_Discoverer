d42443070d10b8b09e09abbc87d4798a662835b8
Yosef <mercytv932@gmail.com>
2026-09-14 14:31:21 -0400

    creating the ErrorMessage component

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,5 +1,10 @@
 function ErrorMessage() {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Error loading recipes!</h3>
+      <p>Please wait a moment and try again.</p>
+    </div>
+  );
 }
 
 export default ErrorMessage;
