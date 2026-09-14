118319cdb25c5cb575cf6e3072fb7b52723b0559
Yosef <mercytv932@gmail.com>
2026-09-14 19:05:24 -0400

    done with local storage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+function useLocalStorage<T>(key: string, initialValue: T) {
+  const [favorites, setFavorites] = useState<T>(() => {
+    const saved = localStorage.getItem(key);
+    return saved ? JSON.parse(saved) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(favorites));
+  }, [key, favorites]);
+  return [favorites, setFavorites];
+}
+
+export default useLocalStorage;
