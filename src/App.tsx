9c0429e111d2df547f011ab471f32f6d802e0909
Yosef <mercytv932@gmail.com>
2026-09-15 16:18:20 -0400

    installing and seting up tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App() {
   return (
     <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
