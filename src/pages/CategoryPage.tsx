9b1d5b9abda13be2b08174a37e17534378a132a7
Yosef <mercytv932@gmail.com>
2026-09-15 14:13:27 -0400

    Category function working

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,5 +1,42 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+
+type Recipe = {
+  idMeal: string;
+  strMeal: string;
+  strMealThumb: string;
+};
+
+type RecipeResponse = {
+  meals: Recipe[] | null;
+};
 function CategoryPage() {
-  return <div></div>;
+  const { categoryName } = useParams();
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`;
+  const { data, loading, error } = useFetch<RecipeResponse>(url);
+
+  if (loading) {
+    return <p>Loading recipes...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div>
+      <h2> {categoryName} Recipes</h2>
+
+      <div>
+        {data?.meals?.map((recipe) => (
+          <div key={recipe.idMeal}>
+            <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+            <h3>{recipe.strMeal}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CategoryPage;
